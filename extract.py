f4e2f35871bbe4a7f0c1c029239d1e48e9ba02f6
p <p>
2023-02-05 11:12:01 -0800

    small upgrades

--- extract.py
+++ extract.py
@@ -1,6 +1,7 @@
 
 import glob
 import json
+import shutil
 import subprocess
 import re
 import os
@@ -14,9 +15,8 @@ def extract_loop(n: int):
     done = glob.glob(f'{fp}/*.json')
     files = list(itertools.chain.from_iterable([glob.glob(f'{fp}/*.{ext}') for ext in extensions]))
 
-    replace_ext = lambda x: x.replace('.pdf', '.json').replace('.png', '.json').replace('.jpg', '.json').replace('.jpeg', '.json')
-    commands = [f'''./OCR en false true "{file}" "{replace_ext(file)}"''' for file in files]
-    commands = [command for command, file in zip(commands, files) if replace_ext(file) not in done]
+    commands = [f'''./OCR en false true "{file}" "{file + '.json'}"''' for file in files if ';' not in file and '"' not in file]
+    commands = [command for command, file in zip(commands, files) if f'{file + ".json"}' not in done]
 
     print(f'Running {len(commands)} commands')
     for j in range(max(int(len(commands)/n), 1)):
@@ -27,7 +27,7 @@ def extract_loop(n: int):
         print(f'Batch complete. Error count: {sum([p.returncode for p in procs])}')
 
 
-def json_info(regex: re.Pattern, dedup: bool = False) -> None:
+def json_info(regex: re.Pattern, dedup: bool = False, copy_to_path: str = None) -> None:
     json_files = glob.glob(f'{fp}/*.json')
     token_set = set()
     for json_file in json_files:
@@ -36,12 +36,25 @@ def json_info(regex: re.Pattern, dedup: bool = False) -> None:
             if 'text' in t:
                 for line in t['text'].split('\n'):
                     tokens = re.findall(regex, line)
+                    copy_path = f'{copy_to_path}/{os.path.basename(json_file)[:-5]}'
+                    if copy_path is not None and len(tokens) and not os.path.isfile(copy_path):
+                        shutil.copy(json_file[:-5], copy_path)
                     for token in tokens:
                         if not dedup:
                             print(json_file + ": " + token)
                         token_set.add(token)
     if dedup:
         print('\n'.join(token_set))
+
+
+def fix_outdated_json():
+    for json_file in glob.glob(f'{fp}/*.json'):
+        file_ext = [os.path.isfile(json_file[:-4] + ext) for ext in extensions]
+
+        for i, ext_correct in enumerate(file_ext):
+            if ext_correct:
+                os.rename(json_file, json_file[:-4] + extensions[i] + '.json')
+                break
 
 
 def main():
@@ -51,7 +64,7 @@ def main():
 
     email_re = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
     # ip_re = re.compile(r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})")
-    json_info(email_re, dedup=True)
+    json_info(email_re, copy_to_path="search/emails")
 
 
 if __name__ == '__main__':
